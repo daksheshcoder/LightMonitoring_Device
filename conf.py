cb5dfe58d779d76e68f08eb9501082b867758ddf
dakshesh_ch <76215048+daksheshcoder@users.noreply.github.com>
2022-03-26 17:10:36 +0530

    Update conf.py

--- conf.py
+++ conf.py
@@ -1,6 +1,6 @@
-API_KEY = '74f64958-8224-4223-ba61-18c484d97c8b'
-DEVICE_ID='BOLT14001217'
+API_KEY = 'xxxxxxxxxxxxxx'
+DEVICE_ID='xxxxxxxxxxxxxxx'
 telegram_chat_id = "@bolt_iotprojets"             
-telegram_bot_id = "bot2081587738:AAEsgIuiDuDomQCQfwNhMs2NZ3W_P_y_evc" 
+telegram_bot_id = "xxxxxxxxxxxxxxxxxx" 
 FRAME_SIZE = 10
 MUL_FACTOR = 3
